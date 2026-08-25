2541b457ebabb2abd8cb59b9fcd20e964d10c229
Aditya Kumar Sah <adityakumarsah8709@gmail.com>
2026-08-25 18:40:46 +0530

    fix: Limit COOP/COEP headers only to compiler route

--- BCE/next.config.ts
+++ BCE/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/code-arena/:path*',
+        source: '/code-arena/compiler',
         headers: [
           { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' }
